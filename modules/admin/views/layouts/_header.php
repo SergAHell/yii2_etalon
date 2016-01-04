6fa56296992872f6246e0256e86f64d24fc9c820
Serg <serg.a.hell@gmail.com>
2016-01-04 22:28:07 +0300

    fix

--- modules/admin/views/layouts/_header.php
+++ modules/admin/views/layouts/_header.php
@@ -1,7 +1,7 @@
 <header class="main-header">
 
 	<?php if (!Yii::$app->user->isGuest) {?>
-	<a href="index2.html" class="logo">
+	<a href="/" class="logo">
 		<span class="logo-mini"><b>A</b>LT</span>
 		<span class="logo-lg"><b>Admin</b>LTE</span>
 	</a>
