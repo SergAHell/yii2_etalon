9917c6c56a9fab543f602b3a403b195f1db4c970
Sergey Konoplyanko <serg@tviz.tv>
2016-01-28 16:08:14 +0300

    AdminLTE

--- modules/admin/controllers/CityController.php
+++ modules/admin/controllers/CityController.php
@@ -3,11 +3,6 @@
 namespace app\modules\admin\controllers;
 
 use Yii;
-use app\models\City;
-use app\models\CitySearch;
-use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
 use app\modules\admin\base\BaseController;
 
 class CityController extends BaseController
